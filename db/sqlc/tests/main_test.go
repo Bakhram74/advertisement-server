0bb43df0abfef237fcd5bca2e9d7c3baa9913648
Bakhram <bakhram7493@gmail.com>
2023-09-09 19:46:16 +0300

    fix test

--- db/sqlc/tests/main_test.go
+++ db/sqlc/tests/main_test.go
@@ -2,9 +2,9 @@ package tests
 
 import (
 	"context"
+	"fmt"
 	db "github.com/Bakhram74/advertisement-server.git/db/sqlc"
 	"github.com/Bakhram74/advertisement-server.git/internal/config"
-	"github.com/Bakhram74/advertisement-server.git/pkg/utils"
 	"github.com/jackc/pgx/v5"
 	"log"
 	"os"
@@ -15,11 +15,15 @@ var testQueries *db.Queries
 
 func TestMain(m *testing.M) {
 	cfg := config.GetConfig()
-	conn, err := pgx.Connect(context.Background(), utils.GetPostgresUrl(cfg.Storage))
+	conn, err := pgx.Connect(context.Background(), postgresUrl(cfg.Storage))
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
 	defer conn.Close(context.Background())
 	testQueries = db.New(conn)
 	os.Exit(m.Run())
+}
+
+func postgresUrl(cfg config.StorageConfig) string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.Database, cfg.SSLMode)
 }
